ccddcb7cb623f3b7f641b508fd86073716fd9e16
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2020-05-23 20:08:40 +0200

    Initial Commit

---
+++ read-darkmode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  read-darkmode
+//
+//  Created by Filippo Tortomasi on 2020-05-23.
+//  Copyright © 2020 Filippo Tortomasi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
